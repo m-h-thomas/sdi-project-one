939ffa56cc72e2203c1a0eace8a14fae38f2e2c3
m-h-thomas <mhthomas.8193@gmail.com>
2025-02-14 09:43:32 -0700

    added functionality to category buttons to display all items from that category

--- js/main.js
+++ js/main.js
@@ -1,71 +1,156 @@
 // console.log('My script is running');
 
-displayGallery();
-
 document.querySelector('#search-button').addEventListener("click", getEntry)
 
-function getEntry(entryId){
+function getEntry(){
   let entry = document.querySelector('#search-input').value;
 
-  let encodedEntry = encodeURIComponent(entryId);
+  let encodedEntry = encodeURIComponent(entry); 
 
   fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${encodedEntry}`)
     .then((response) => response.json())
     .then((compendiumData) => {
-
-      let commonLocationsHTML = compendiumData.data['common_locations'].map(location => `<p>${location}</p>`).join('');
-
       document.querySelector('.displayed-entries').innerHTML = `
       <div>
         <img src="${compendiumData.data.image}" alt="${compendiumData.data.name}"/>
       </div>
       <div class="entry-info">
         <h3>${compendiumData.data.name}</h3>
-        <p>Description: ${compendiumData.data.description}</p>
-      </div>
-      <div class="entry-region">
-        <h4>Common Locations:</h4>
-        ${commonLocationsHTML}
+        <p class="description">Description: ${compendiumData.data.description}</p>
       </div>
     `;
     })
 }
 
-function displayGallery() {
-  fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/all')
+document.querySelector('#creature-button').addEventListener('click', getCreatures)
+
+function getCreatures(){
+  fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/creatures`)
     .then((response) => response.json())
-    .then((allCompendiumData) => {
-      let entriesHTML = '';
+    .then((allData) => {
 
-      // Sort entries by id in ascending order
-      const sortedEntries = allCompendiumData.data.sort((a, b) => a.id - b.id);
+      let creaturesHTML = '';
 
-      // Loop through each item in the sorted array and generate HTML
-      sortedEntries.forEach(entry => {
-        entriesHTML += `
-          <div class="entry" data-id="${entry.id}">
-            <img src="${entry.image}" alt="${entry.name}" class="entry-image"/>
-            <h5>${entry.name}</h5>
-            <p>${entry.id}</p>
+      allData.data.forEach((creature) => {
+        creaturesHTML += `
+          <div>
+            <img src="${creature.image}" alt="${creature.name}"/>
           </div>
-        `;
-      });
+          <div class="entry-info">
+            <h4>${creature.name}</h4>
+            <p class="description">${creature.description}</p>
+          </div>
+        `
+      })
 
-      // Insert the generated HTML into the all-entries container
-      document.querySelector('.all-entries').innerHTML = entriesHTML;
-
-      // Now that the entries are in the DOM, attach event listeners to the images
-      document.querySelectorAll('.entry-image').forEach(image => {
-        image.addEventListener('click', (eventObj) => {
-          let entryId = eventObj.target.closest('.entry').getAttribute('data-id');
-          getEntry(entryId);
-        });
-      });
+      document.querySelector('.displayed-entries').innerHTML = creaturesHTML;
+      
     })
 }
 
+document.querySelector('#monster-button').addEventListener('click', getMonsters)
 
+function getMonsters(){
+  fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters`)
+    .then((response) => response.json())
+    .then((allData) => {
 
+      let monstersHTML = '';
+
+      allData.data.forEach((monster) => {
+        monstersHTML += `
+          <div>
+            <img src="${monster.image}" alt="${monster.name}"/>
+          </div>
+          <div class="entry-info">
+            <h4>${monster.name}</h4>
+            <p class="description">${monster.description}</p>
+          </div>
+        `
+      })
+
+      document.querySelector('.displayed-entries').innerHTML = monstersHTML;
+      
+    })
+}
+
+document.querySelector('#materials-button').addEventListener('click', getMaterials)
+
+function getMaterials(){
+  fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/materials`)
+    .then((response) => response.json())
+    .then((allData) => {
+
+      let materialsHTML = '';
+
+      allData.data.forEach((material) => {
+        materialsHTML += `
+          <div>
+            <img src="${material.image}" alt="${material.name}"/>
+          </div>
+          <div class="entry-info">
+            <h4>${material.name}</h4>
+            <p class="description">${material.description}</p>
+          </div>
+        `
+      })
+
+      document.querySelector('.displayed-entries').innerHTML = materialsHTML;
+      
+    })
+}
+
+document.querySelector('#equipment-button').addEventListener('click', getEquipment)
+
+function getEquipment(){
+  fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/equipment`)
+    .then((response) => response.json())
+    .then((allData) => {
+
+      let equipmentHTML = '';
+
+      allData.data.forEach((item) => {
+        equipmentHTML += `
+          <div>
+            <img src="${item.image}" alt="${item.name}"/>
+          </div>
+          <div class="entry-info">
+            <h4>${item.name}</h4>
+            <p class="description">${item.description}</p>
+          </div>
+        `
+      })
+
+      document.querySelector('.displayed-entries').innerHTML = equipmentHTML;
+      
+    })
+}
+
+document.querySelector('#treasure-button').addEventListener('click', getTreasure)
+
+function getTreasure(){
+  fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/treasure`)
+    .then((response) => response.json())
+    .then((allData) => {
+
+      let treasureHTML = '';
+
+      allData.data.forEach((item) => {
+        treasureHTML += `
+          <div>
+            <img src="${item.image}" alt="${item.name}"/>
+          </div>
+          <div class="entry-info">
+            <h4>${item.name}</h4>
+            <p class="description">${item.description}</p>
+          </div>
+        `
+      })
+
+      document.querySelector('.displayed-entries').innerHTML = treasureHTML;
+      
+    })
+}
 
 /*
 
